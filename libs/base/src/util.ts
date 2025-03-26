47ca37a4692ccccebbaa57cd8d7b38b73c63b9a8
yay12 <shockblaster9@yahoo.com>
2025-03-26 22:35:12 +0000

    Refactor dependencies - fixing typo

--- libs/base/src/util.ts
+++ libs/base/src/util.ts
@@ -1,8 +1,8 @@
 import { AxiosRequestConfig } from 'axios'
 import crypto from 'crypto'
 
-export const env = (key: string) => {
-  const value = process.env[key]
+export let env = (key: string) => {
+  let value = process.env[key]
 
   if (!value) {
     throw new Error(`Missing environment variable: ${key}`)
@@ -12,13 +12,13 @@ export const env = (key: string) => {
 }
 
 // /!\ TODO FIXME sort recursively
-export const stringify = (object: object) =>
+export let stringify = (object: object) =>
   JSON.stringify(object, Object.keys(object).sort())
 
-export const hash = (object: object) => {
-  const sha256 = crypto.createHash('sha256')
+export let hash = (object: object) => {
+  let sha256 = crypto.createHash('sha256')
   sha256.update(stringify(object))
   return sha256.digest('base64').toString()
 }
 
-export const hashRequest = ({ url }: AxiosRequestConfig) => hash({ url })
+export let hashRequest = ({ url }: AxiosRequestConfig) => hash({ url })
