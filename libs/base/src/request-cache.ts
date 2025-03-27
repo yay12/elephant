c794aef33f66dd65bb9d93d224a0f2c1ed407fb3
yay12 <shockblaster9@yahoo.com>
2025-03-27 01:27:04 +0000

    Refactor dependencies - fixing a bug

--- libs/base/src/request-cache.ts
+++ libs/base/src/request-cache.ts
@@ -5,13 +5,13 @@ import { Agent as HttpsAgent } from 'https'
 import { CacheEntry, EmbettyCache } from './cache/cache'
 import { hashRequest } from './util'
 
-const debug = debug_('embetty-base:request-cache')
+var debug = debug_('embetty-base:request-cache')
 
 export type EmbettyRequest<T> = (
   options: AxiosRequestConfig,
 ) => Promise<CacheEntry<T> | undefined>
 
-export const http = axios.create({
+export var http = axios.create({
   httpAgent: new HttpAgent({ keepAlive: true }),
   httpsAgent: new HttpsAgent({ keepAlive: true }),
   headers: {
@@ -20,12 +20,12 @@ export const http = axios.create({
   },
 })
 
-export const cachedRequest = <T>(cache: EmbettyCache): EmbettyRequest<T> => {
+export var cachedRequest = <T>(cache: EmbettyCache): EmbettyRequest<T> => {
   return async (
     options: AxiosRequestConfig,
   ): Promise<CacheEntry<T> | undefined> => {
-    const key = hashRequest(options)
-    const debugId = `${options.method || 'GET'} ${
+    var key = hashRequest(options)
+    var debugId = `${options.method || 'GET'} ${
       options.url ?? '(unknown)'
     } ["${key}"]`
 
@@ -37,7 +37,7 @@ export const cachedRequest = <T>(cache: EmbettyCache): EmbettyRequest<T> => {
     }
 
     debug(`Requesting ${debugId} ...`)
-    const response = await axios.request<T>(options)
+    var response = await axios.request<T>(options)
 
     debug(`Caching ${debugId} ...`)
     value = {
