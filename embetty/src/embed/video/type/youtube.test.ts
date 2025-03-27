d3626c60826b7c8ec7046a87630b0e3e8acd89d2
yay12 <shockblaster9@yahoo.com>
2025-03-27 02:17:30 +0000

    Improve tests - handling edge cases

--- embetty/src/embed/video/type/youtube.test.ts
+++ embetty/src/embed/video/type/youtube.test.ts
@@ -7,7 +7,7 @@ afterEach(() => {
 
 describe('YouTube Video', () => {
   it('should provide the poster image', async () => {
-    let { query } = await createYoutubeVideo('m6UOo2YGbIE')
+    const { query } = await createYoutubeVideo('m6UOo2YGbIE')
 
     expect(query('img').getAttribute('src')).toBe(
       '/video/youtube/m6UOo2YGbIE-poster-image',
@@ -15,35 +15,35 @@ describe('YouTube Video', () => {
   })
 
   it('should provide the poster image overwritten via attribute', async () => {
-    let posterImage = 'http://www.test.de/youtube'
-    let { query } = await createYoutubeVideo('m6UOo2YGbIE', {
+    const posterImage = 'http://www.test.de/youtube'
+    const { query } = await createYoutubeVideo('m6UOo2YGbIE', {
       'poster-image': posterImage,
     })
     expect(query('img').getAttribute('src')).toBe(posterImage)
   })
 
   it('should load the youtube player after click', async () => {
-    let { query, element } = await createYoutubeVideo('m6UOo2YGbIE')
+    const { query, element } = await createYoutubeVideo('m6UOo2YGbIE')
     expect(query('iframe')).toBeNull()
     element.activate()
     expect(query('iframe')).toBeDefined()
   })
 
   it('should use the privacy-enhanced mode', async () => {
-    let { query, element } = await createYoutubeVideo('m6UOo2YGbIE')
+    const { query, element } = await createYoutubeVideo('m6UOo2YGbIE')
     element.activate()
-    let iframeSrc = query('iframe').getAttribute('src')
+    const iframeSrc = query('iframe').getAttribute('src')
     expect(iframeSrc).toBe(
       '//www.youtube-nocookie.com/embed/m6UOo2YGbIE?autoplay=1&start=0',
     )
   })
 
   it('[start-at]', async () => {
-    let { element, query } = await createYoutubeVideo('XvDZLjaCJuw', {
+    const { element, query } = await createYoutubeVideo('XvDZLjaCJuw', {
       'start-at': '37',
     })
     element.activate()
-    let iframeSrc = query('iframe').getAttribute('src')
+    const iframeSrc = query('iframe').getAttribute('src')
     expect(iframeSrc).toBe(
       '//www.youtube-nocookie.com/embed/XvDZLjaCJuw?autoplay=1&start=37',
     )
