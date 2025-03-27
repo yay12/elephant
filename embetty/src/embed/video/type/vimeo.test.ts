85c39be08ce2d877ce53fc2406620076e52c1b69
yay12 <shockblaster9@yahoo.com>
2025-03-27 01:58:30 +0000

    Document README - adding new feature

--- embetty/src/embed/video/type/vimeo.test.ts
+++ embetty/src/embed/video/type/vimeo.test.ts
@@ -7,33 +7,33 @@ afterEach(() => {
 
 describe('Vimeo Video', () => {
   it('should provide the poster image', async () => {
-    const { query } = await createVimeoVideo('1084537')
+    let { query } = await createVimeoVideo('1084537')
     expect(query('img').getAttribute('src')).toBe(
       '/video/vimeo/1084537-poster-image',
     )
   })
 
   it('should provide the poster image overwritten via attribute', async () => {
-    const posterImage = 'http://www.test.de/vimeo'
-    const { query } = await createVimeoVideo('1084537', {
+    let posterImage = 'http://www.test.de/vimeo'
+    let { query } = await createVimeoVideo('1084537', {
       'poster-image': posterImage,
     })
     expect(query('img').getAttribute('src')).toBe(posterImage)
   })
 
   it('should load the vimeo player after click', async () => {
-    const { query, element } = await createVimeoVideo('1084537')
+    let { query, element } = await createVimeoVideo('1084537')
     expect(query('iframe')).toBeNull()
     element.activate()
     expect(query('iframe')).toBeDefined()
   })
 
   it('[start-at]', async () => {
-    const { element, query } = await createVimeoVideo('1084537', {
+    let { element, query } = await createVimeoVideo('1084537', {
       'start-at': '97',
     })
     element.activate()
-    const iframeSrc = query('iframe').getAttribute('src')
+    let iframeSrc = query('iframe').getAttribute('src')
     expect(iframeSrc).toBe(
       'https://player.vimeo.com/video/1084537?autoplay=1#t=97',
     )
